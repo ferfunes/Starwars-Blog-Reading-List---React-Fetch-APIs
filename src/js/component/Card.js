d898fd9fe7a7e47c239a9a0f9ff771a9c461ed0f
Fernando Funez <ferfunes@gmail.com>
2019-09-28 19:04:00 +0000

    added fetch

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -1,16 +1,40 @@
 import React from "react";
 
-export const Card = () => {
-	return (
-		<div className="card col-lg-3 col-md-6 mb-4 ">
-			<div className="card-body">
-				<h5 className="card-title">Card title</h5>
-				<h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-				<p className="card-text">
-					Some quick example text to build on the card title and make
-					up the bulk of the cards content.
-				</p>
+export class Card extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			planet: []
+		};
+	}
+
+	componentDidMount() {
+		fetch("https://swapi.co/api/planets/")
+			.then(response => response.json())
+			.then(data => {
+				let { planet } = this.state;
+				planet.planets = data.results;
+				this.setState({ planet: data.results[0] });
+				console.log(data);
+			});
+	}
+	render() {
+		return (
+			<div className="card col-lg-3 col-md-6 mb-4 ">
+				<div className="card-body">
+					<h5 className="card-title">Planets</h5>
+					<h6 className="card-subtitle mb-2 text-muted">
+						Name: {this.state.planet.name}
+					</h6>
+					<h6 className="card-subtitle mb-2 text-muted">
+						Terrain: {this.state.planet.terrain}
+					</h6>
+					<h6 className="card-subtitle mb-2 text-muted">
+						Climate: {this.state.planet.climate}
+					</h6>
+				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+}
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -6,44 +6,12 @@ import { Card } from "./Card.js";
 
 //create your first component
 export class Home extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			items: [],
-			isLoaded: false
-		};
-	}
-
-	componentDidMount() {
-		fetch("https://jsonplaceholder.typicode.com/users")
-			.then(res => res.json())
-			.then(json => {
-				this.setState({
-					isLoaded: true,
-					items: json
-				});
-			});
-	}
-
 	render() {
-		let { isLoaded, items } = this.state;
-
-		if (!isLoaded) {
-			return <div> Loading...</div>;
-		} else {
-			return (
-				<div className="home">
-					<NavBar />
-					<Card />
-					<ul>
-						{items.map(data => (
-							<li key={data.name}>
-								Name: {data.name} | Mass: {data.email}
-							</li>
-						))}
-					</ul>
-				</div>
-			);
-		}
+		return (
+			<div className="home">
+				<NavBar />
+				<Card />
+			</div>
+		);
 	}
 }
